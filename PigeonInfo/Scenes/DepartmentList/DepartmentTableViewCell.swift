fd26e6818be27e35450c9078aef8666d9a552fb5
m4riusz <mariusz.sut.94+github@gmail.com>
2020-04-22 21:22:16 +0200

    list cell wip

---
+++ PigeonInfo/Scenes/DepartmentList/DepartmentTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DepartmentTableViewCell.swift
+//  PigeonInfo
+//
+//  Created by Mariusz Sut on 22/04/2020.
+//  Copyright © 2020 Mariusz Sut. All rights reserved.
+//
+
+import UIKit
+
+final class DepartmentTableViewCell: UITableViewCell {
+    
+}
